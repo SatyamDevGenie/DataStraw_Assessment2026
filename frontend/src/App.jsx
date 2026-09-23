b44f5eaed89989d5003d36161824c47c40ddd269
SatyamDevGenie <satyamsawant54@gmail.com>
2026-09-24 03:22:06 +0530

    Setup + Install TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-extrabold text-blue-600 mb-4">
+        Start with frontend now for DataStraw Assessment!
+      </h1>
+      <p className="text-lg text-gray-700 bg-white p-4 rounded-lg shadow-md">
+        If you see this styled card and large blue text, your setup is correct.
+      </p>
+    </div>
+  );
+}
